acd40580edf3c434ed2188de1ad6c2f6d2caf701
diver2daze <dkurishev@gmail.com>
2015-06-20 17:39:44 +0300

    add zabbix API: fixed

--- monitor.py
+++ monitor.py
@@ -9,16 +9,18 @@ from pyzabbix import ZabbixAPI
 
 
 if __name__ == "__main__":
-    message ={}
     zapi = ZabbixAPI("http://ni2.codeabovelab.com:8080/zabbix")
     zapi.login("admin", "zabbix")
     print "Connected to Zabbix API Version %s" % zapi.api_version()
 
-    for host in zapi.host.get(output="extend"):
-        message[host['hostid']]=zapi.item.get(hostid=host['hostid'])
 
-    #print json.dumps(message, indent=2)
+
     rabbit_connection = pika.BlockingConnection(pika.URLParameters('amqp://admin:opentsp@ni1.codeabovelab.com:5672/%2F'))
+    print "Connected to rabbit %s"% rabbit_connection.server_properties
     channel = rabbit_connection.channel()
     channel.exchange_declare(exchange='opentsp.log', type='fanout')
-    channel.basic_publish(exchange='opentsp.log', routing_key='', body=json.dumps(message, indent=2))
+    for host in zapi.host.get(output="extend"):
+        for item in zapi.item.get(hostid=host['hostid']):
+            channel.basic_publish(exchange='opentsp.log', routing_key='zabbix', body=json.dumps(item), properties=pika.BasicProperties(content_type='application/json'))
+
+    rabbit_connection.close()
